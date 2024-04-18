8158a97d4e948b4195d73342ea1c9a3c5b4b8459
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-04-18 13:05:35 +0700

    feat: update brand page

--- hokaidovietnam_fontend_client/src/Components/Banner/index.tsx
+++ hokaidovietnam_fontend_client/src/Components/Banner/index.tsx
@@ -1,28 +1,39 @@
-import { ReactNode } from 'react'
-import Background from 'assets/image/banner.png';
+import { ReactNode } from "react";
+import Background from "assets/image/banner.png";
+import GrungeSVG from "../GrungeSVG/GrungeSVG";
 
 interface IProps {
-    title: string | ReactNode;
-    background?: string
+  title: string | ReactNode;
+  background?: string;
 }
 function Banner(props: IProps) {
-    const { title, background = Background } = props;
+  const { title, background = Background } = props;
 
-    return (
-        <div style={{
-            height: "208px",
-            width: "100vw",
-            backgroundImage: `url(${background})`,
-            display: 'flex',
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundSize: 'cover'
-        }}>
-            <h1 style={{
-                fontSize: 36
-            }}>{title}</h1>
-        </div>
-    )
+  return (
+    <>
+      <div
+        className={`bannerBrandRespon relative`}
+        style={{
+          height: "208px",
+          width: "100vw",
+          backgroundImage: `url(${background})`,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <h1
+          className={`
+          text-base 
+          sm:text-4xl 
+          tagH1BrannerRespon`}
+        > {title} </h1>
+
+        <GrungeSVG />
+      </div>
+    </>
+  );
 }
 
-export default Banner
+export default Banner;
