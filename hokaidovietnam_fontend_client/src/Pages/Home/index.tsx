86afa56139bd350a1d1f1d75c14ff83230428a56
unknown <tupm@devcamp.edu.vn>
2024-04-20 15:33:21 +0700

    media-page

--- hokaidovietnam_fontend_client/src/Pages/Home/index.tsx
+++ hokaidovietnam_fontend_client/src/Pages/Home/index.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import img from '../../assets/image/slider01.png'
-
-
-
+import React from "react";
+import img from "../../assets/image/slider01.png";
 
 export default function Home() {
-    return (
-        <div className="">
-            <img style={{
-                width: "100%"
-            }} src={img} alt="..." />
-            <img style={{
-                width: "100%"
-            }} src={img} alt="..." />
-            <img style={{
-                width: "100%"
-            }} src={img} alt="..." />
-
-        </div>
-    )
+  return (
+    <div className="">
+      <img
+        style={{
+          width: "100%",
+        }}
+        src={img}
+        alt="..."
+      />
+      <img
+        style={{
+          width: "100%",
+        }}
+        src={img}
+        alt="..."
+      />
+      <img
+        style={{
+          width: "100%",
+        }}
+        src={img}
+        alt="..."
+      />
+      <div>qưeqwewqeqwe</div>
+    </div>
+  );
 }
